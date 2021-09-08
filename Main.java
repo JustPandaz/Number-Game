95ad6dd172f2b37d538855d15a287846d61026aa
JustPandaz <83378273+JustPandaz@users.noreply.github.com>
2021-09-08 12:52:33 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
-public class Main {
-    @SuppressWarnings("unused")
-    public static void main(String[] args) {
+package nbguesser.panda.org;
 
-        new SystemUI();
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("guesser.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Guess The Number!");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
